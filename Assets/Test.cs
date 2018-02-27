811046de2567a908b9493599a723b950bbea62cf
Morrila <alexander.wilson@sky.uk>
2018-02-27 15:43:21 +0000

    removed redundant using statements

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,16 +1,13 @@
 ﻿using System.IO;
-using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public static class Test
 {
     public static void PreBuildTest()
     {
         EditorSceneManager.OpenScene(Path.Combine(Application.dataPath, "test.unity"), OpenSceneMode.Single);
-        GameObject.Find("Text").GetComponent<Text>().text = "Hello";
+        //GameObject.Find("Text").GetComponent<Text>().text = "Hello";
         EditorSceneManager.SaveOpenScenes();
     }
 }
