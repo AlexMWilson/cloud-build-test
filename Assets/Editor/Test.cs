189dbcf79c909fe04d4a7526df54232fb6330e07
Morrila <alexander.wilson@sky.uk>
2018-02-27 16:13:27 +0000

    moved prebuild to editor script

---
+++ Assets/Editor/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class Test
+{
+    public static void PreBuildTest()
+    {
+        EditorSceneManager.OpenScene(Path.Combine(Application.dataPath, "test.unity"), OpenSceneMode.Single);
+        GameObject.Find("Text").GetComponent<Text>().text = "Hello";
+        EditorSceneManager.SaveOpenScenes();
+    }
+}
+  
